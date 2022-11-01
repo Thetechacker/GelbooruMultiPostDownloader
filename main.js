e1b7478f0297bd3579ec2c7cdaf001854134e631
Thetechacker <thetechacker@outlook.com>
2022-11-01 18:45:32 +0100

    NNN Update v1.0.1
    NNN Update v1.0.1
    
    - Added "Disable NNN Block :/" Switch Button
    - Added "Autom. close post tabs" Switch Button

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ const infobox = document.getElementById("infobox")
 const cpgl = document.getElementById("cpgl")
 const dgpi = document.getElementById("dgpi")
 const closetabs = document.getElementById("closetabs")
+const autoclosetabs = document.getElementById("autoclosetabs")
+const nnnblock = document.getElementById("nnnblock")
+
+chrome.storage.local.get(null, options => {
+    autoclosetabs.checked = options.autoclosetabs
+    nnnblock.checked = !options.nnnblock
+})
 
 port.onMessage.addListener(message => {
     if(infobox.classList.contains("error")) infobox.classList.remove("error")
@@ -11,19 +18,25 @@ port.onMessage.addListener(message => {
     if(message.type === "error"){
         infobox.classList.add("error")
         infobox.innerText = message.data.err
+
+        window.scrollTo(0, document.body.scrollHeight);
     } else if(message.type === "action"){
         if(message.action === "setInfobox"){
             infobox.innerText = message.data.content
+
+            window.scrollTo(0, document.body.scrollHeight);
         } else if(message.action === "clipboardWriteText"){
             navigator.clipboard.writeText(message.data.content)
         }
     }
 })
 
-function tabOperation(type){
-    port.postMessage({ type })
+function tabOperation(type, data){
+    port.postMessage({ type, data })
 }
 
 cpgl.addEventListener("click", (ev) => tabOperation("cpgl"))
 dgpi.addEventListener("click", (ev) => tabOperation("dgpi"))
 closetabs.addEventListener("click", (ev) => tabOperation("closetabs"))
+autoclosetabs.addEventListener("change", (ev) => tabOperation("autoclosetabs_swchange", autoclosetabs.checked))
+nnnblock.addEventListener("change", (ev) => tabOperation("nnnblock_swchange", nnnblock.checked))
--- worker.js
+++ worker.js
@@ -19,7 +19,7 @@ function parseURIParameters(uri){
 chrome.runtime.onConnect.addListener(port => {
     port.onMessage.addListener(message => {
         chrome.tabs.query({}).then(tabs => {
-            tabs = tabs.filter(tab => {
+            var posttabs = tabs.filter(tab => {
                 const up = parseURIParameters(tab.url)
     
                 if(tab.url.startsWith(baseuri) && (up.page === "post") && (up.s === "view") && (!isNaN(up.id))){
@@ -28,38 +28,68 @@ chrome.runtime.onConnect.addListener(port => {
                     return false
                 }
             })
+
+            if(message.type === "autoclosetabs_swchange"){
+                chrome.storage.local.set({ autoclosetabs: message.data })
+            } else if(message.type === "nnnblock_swchange"){
+                chrome.storage.local.set({ nnnblock: !message.data })
+
+                if(!message.data){
+                    chrome.tabs.query({}).then(tabs => tabs.filter(tab => tab.url.startsWith(baseuri)).forEach(tab => chrome.tabs.remove(tab.id)))
+                }
+            } else {
+                chrome.storage.local.get(null, options => {
+                    if(options.nnnblock){
+                        const curYear = new Date().getFullYear()
+    
+                        if((new Date(`1 November ${curYear}`) < new Date()) && (new Date(`31 November ${curYear}`) > new Date())){
+                            const errMessage = "Nu-uh :/"
+                            port.postMessage({ type: "error", data: { err: errMessage, errStack: errMessage } })
+                            tabs.filter(tab => tab.url.startsWith(baseuri)).forEach(tab => chrome.tabs.remove(tab.id))
             
-            const curYear = new Date().getFullYear()
+                            return
+                        }
+                    }
+    
+                    if(!posttabs.length) return port.postMessage({ type: "action", action: "setInfobox", data: { content: "No Gelbooru post tabs found." } })
+    
+                    if(message.type === "cpgl"){
+                        port.postMessage({ type: "action", action: "clipboardWriteText", data: { content: posttabs.map(tab => tab.url).join("\n") } })
+                        port.postMessage({ type: "action", action: "setInfobox", data: { content: "Gelbooru post links copied to clipboard ;>>" } })
+                    } else if(message.type === "dgpi"){
+                        posttabs = [...new Set(posttabs.map(tab => parseURIParameters(tab.url).id))]
+                        
+                        posttabs.forEach(id => {
+                            fetch(`${baseuri}/index.php?page=dapi&s=post&q=index&id=${id}&json=1`, { method: "GET" }).then(res => res.json()).then(res => {
+                                const image_uri = res.post[0].file_url
+                                const image_ext = image_uri.substring(image_uri.lastIndexOf('/') + 1).split(".")
+        
+                                chrome.downloads.download({ url: image_uri, filename: `gelbooru_${res.post[0].id}${image_ext[1] ? `.${image_ext[1]}` : ""}` })
+                            }).catch(err => port.postMessage({ type: "error", data: { err: err.toString(), errStack: err.stack.toString() } }))
+                        })
+        
+                        port.postMessage({ type: "action", action: "setInfobox", data: { content: `Downloading ${posttabs.length} post images...` } })
+                    } else if(message.type === "closetabs"){
+                        posttabs.forEach(tab => chrome.tabs.remove(tab.id))
+        
+                        port.postMessage({ type: "action", action: "setInfobox", data: { content: "Closed all the post tabs." } })
+                    }
 
-            if((new Date(`1 November ${curYear}`) < new Date()) && (new Date(`31 November ${curYear}`) > new Date())){
-                port.postMessage({ type: "error", data: { err: "Try the next month ;)", errStack: "Try the next month ;)" } })
-
-                return
-            }
-
-            if(!tabs.length) return port.postMessage({ type: "action", action: "setInfobox", data: { content: "No Gelbooru post tabs found." } })
-
-            if(message.type === "cpgl"){
-                port.postMessage({ type: "action", action: "clipboardWriteText", data: { content: tabs.map(tab => tab.url).join("\n") } })
-                port.postMessage({ type: "action", action: "setInfobox", data: { content: "Gelbooru post links copied to clipboard ;>>" } })
-            } else if(message.type === "dgpi"){
-                tabs = [...new Set(tabs.map(tab => parseURIParameters(tab.url).id))]
-                
-                tabs.forEach(id => {
-                    fetch(`${baseuri}/index.php?page=dapi&s=post&q=index&id=${id}&json=1`, { method: "GET" }).then(res => res.json()).then(res => {
-                        const image_uri = res.post[0].file_url
-                        const image_ext = image_uri.substring(image_uri.lastIndexOf('/') + 1).split(".")
-
-                        chrome.downloads.download({ url: image_uri, filename: `gelbooru_${res.post[0].id}${image_ext[1] ? `.${image_ext[1]}` : ""}` })
-                    }).catch(err => port.postMessage({ type: "error", data: { err: err.toString(), errStack: err.stack.toString() } }))
+                    if(message.type !== "closetabs" && options.autoclosetabs){
+                        posttabs.forEach(tab => chrome.tabs.remove(tab.id))
+                    }
                 })
-
-                port.postMessage({ type: "action", action: "setInfobox", data: { content: `Downloading ${tabs.length} post images...` } })
-            } else if(message.type === "closetabs"){
-                tabs.forEach(tab => chrome.tabs.remove(tab.id))
-
-                port.postMessage({ type: "action", action: "setInfobox", data: { content: "Closed all the post tabs." } })
             }
         }).catch(err => port.postMessage({ type: "error", data: { err: err.toString(), errStack: err.stack.toString() } }))
+    })
+})
+
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+    if(tab.status !== "loading" || !tab.url.startsWith(baseuri)) return
+
+    chrome.storage.local.get(['nnnblock'], options => {
+        if(options.nnnblock){
+            chrome.tabs.remove(tabId)
+        }
     })
 })
